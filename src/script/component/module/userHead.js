82176d62b309375b623e207ab10ebe9cd7edb09a
sjp1638 <280616734@qq.com>
2017-03-31 20:42:19 +0800

    hami-0301-8

--- src/script/component/module/userHead.js
+++ src/script/component/module/userHead.js
@@ -1,18 +1,13 @@
 import React from "react"
-import logo from './../../../../images/1111.png'
-import soso from './../../../../images/search_ico.png'
-import more from './../../../../images/catgray_w.png'
+import set from './../../../../images/user/set_w.png'
+import more from './../../../../images/user/more_w.png'
 
 
 class boardHead extends React.Component{
 	render(){
 		return (
-			<div className="yo-header yo-header-a">
-	      		<h2 className="title">
-	      			<img src={soso} />
-	      			<span>搜索果蔬、美食、家电、母婴</span>
-	      		</h2>
-	      		<span className="regret"><img src={logo} /></span>
+			<div className="userHead">
+	      		<span className="kindre"><img src={set} /></span>
 	      		<span className="affirm"><img src={more} /></span>
 			</div>
 		)
