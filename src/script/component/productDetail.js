5d365407dba504bc12cdc1ea6538f63069fbae06
Shenjieping <x19940717@126.com>
2017-04-04 22:40:08 +0800

    hami-0404-5

--- src/script/component/productDetail.js
+++ src/script/component/productDetail.js
@@ -1,26 +1,88 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router'
 import { mapStateToProps, mapDispatchToProps } from '../redux/store'
 import Modal from '../../component_dev/modal/src'
+import goods_name_img1 from './../../../images/detail/favorite_r.png'
+import back from './../../../images/detail/arrow_left_r.png'
+import more from './../../../images/detail/more_r.png'
+import check from './../../../images/detail/arrow_right_b.png'
+import color_ok from './../../../images/detail/color-ok.png'
+import color_sale from './../../../images/detail/color-sale.png'
+
 
 import browse_list from "./../../../images/ProductList/browse_list.png"
 class productDetail extends React.Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			title: 'abc',
-			list: "",
-			shows: false
+			product:[],
+			goods_name:[],
+			goods_price:[],
+			goods_marketprice:[],
+			area_name:[],
+			content:[],
+			if_store_cn:[],
+			goods_salenum:[],
+			evaluate_pricent:[]
 		}
+	}
+	back(){
+		window.history.go(-1);
 	}
 	render() {
 		return(
-			<div className="m-productList">
-		        <section>
-		         hahha
-		        
-		        </section>
-		      </div>
+			<div className="m-productDetail">
+		        <div className="bodymo">
+		        	<img src={this.state.product} />
+		        	<p className="goodsName">
+		        		<span>{this.state.goods_name}</span>
+		        		<span><img src={goods_name_img1} /></span>
+		        		
+		        	</p>
+		        </div>
+		        <div className="content">
+		        	<header>
+		        		<span onClick={this.back}><img src={back} /></span>
+		        		<span><img src={more} /></span>
+		        	</header>
+		        	<div className="contener">
+		        		<div className="price">
+		        			<p className="price_s">
+		        				<span>￥</span>
+		        				<span className="pri">{this.state.goods_price}</span>
+		        				<span>￥{this.state.goods_marketprice}</span>
+		        			</p>
+		        			<Link to="" className="price_l">立即抢购</Link>
+		        		</div>
+		        		<div className="check">
+		        			<Link to="">
+			        			<span>查看商品详情</span>
+			        			<img src={check} />
+		        			</Link>
+		        		</div>
+		        		<div className="distri">
+		        			<p>
+		        				<span>送至</span>{this.state.area_name}({this.state.if_store_cn})
+		        				
+		        			</p>
+		        			<p>
+		        				<span>运费</span>{this.state.content},店铺满98免运费
+		        			</p>
+		        		</div>
+		        		<div className="sustain">
+		        			<p><img src={color_ok} />支持随时退款</p>
+		        			<p><img src={color_ok} />支持送货上门</p>
+		        			<p><img src={color_sale} />已售{this.state.goods_salenum}</p>
+		        		</div>
+		        		<div className="evaluate_pricent">
+		        			<Link to="">
+			        			<span></span>
+		        			</Link>
+		        		</div>
+		        	</div>
+		        </div>
+		    </div>
 		)
 	}
 	componentDidMount() {
@@ -36,10 +98,47 @@ class productDetail extends React.Component {
 			}
 			return "";
 		}
-       getUrlSearch(window.location.href,"goods_id")
+      // getUrlSearch(window.location.href,"goods_id")
 		var a= getUrlSearch(window.location.href,"goods_id")
-		console.log(a)
-		fetch('http://www.hamij.com/mobile/index.php?act=goods&op=goods_list&gc_id='+a+'&page=8&curpage=1&gc_id='+a)
+		//console.log(a)
+		fetch("http://www.hamij.com/mobile/index.php?act=goods&op=goods_detail&goods_id="+a+"&key=")
+			.then(function(response){
+				return response.json()
+			}).then(function(json){
+				console.log(json)
+				var list=json;
+				
+				that.setState({
+					product:list.datas.goods_image,
+					goods_name:list.datas.goods_info.goods_name,
+					goods_price:list.datas.goods_info.goods_price,
+					goods_marketprice:list.datas.goods_info.goods_marketprice,
+					area_name:list.datas.goods_hair_info.area_name,
+					content:list.datas.goods_hair_info.content,
+					if_store_cn:list.datas.goods_hair_info.if_store_cn,
+					goods_salenum:list.datas.goods_info.goods_salenum,
+					evaluate_pricent:list.datas.goods_evaluate_info.good_percent
+				})
+				
+			}).catch(function(er){
+				console.log(er)
+			})
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		/*fetch('http://www.hamij.com/mobile/index.php?act=goods&op=goods_list&gc_id='+a+'&page=8&curpage=1&gc_id='+a)
 		
 			.then((res) => {
 				return res.json()
@@ -94,7 +193,7 @@ class productDetail extends React.Component {
 		this.props.onChange({
 			type: 'SETTITLE',
 			title: title
-		})
+		})*/
 	}
 }
 
